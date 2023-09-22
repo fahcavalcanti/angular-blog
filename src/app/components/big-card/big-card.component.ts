85bf297df0b59bc77bf0793ad1edb5cce35ebd4e
Fabricio Cavalcanti <fabriciocavalcanti97@gmail.com>
2023-09-22 17:16:15 -0300

    content created

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -13,6 +13,9 @@ export class BigCardComponent implements OnInit {
   cardTitle:string = ""
   @Input()
   cardDescription:string = ""
+  @Input()
+  id:string = "0"
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Novo Honda Civic 2023: O Epítome da Inovação e Elegância",
+    "description":"A Honda surpreende o mundo automobilístico com o lançamento da próxima geração do Honda Civic. Este ícone dos sedãs compactos evoluiu para um novo patamar, unindo um design revolucionário com funcionalidades tecnológicas de ponta e um desempenho excepcional. O visual do Civic 2023 é uma verdadeira obra de arte, com linhas nítidas, uma grade frontal imponente e um perfil aerodinâmico que não apenas melhora a eficiência, mas também capta olhares onde quer que vá. No interior, a cabine foi cuidadosamente projetada para proporcionar um ambiente de luxo moderno, com materiais de alta qualidade, uma tela de infotainment de fácil acesso e bancos que garantem conforto mesmo nas viagens mais longas. Mas a verdadeira revolução ocorre sob o capô, onde uma variedade de opções de motorização, incluindo motores a gasolina e híbridos, oferecem desempenho excepcional e eficiência de combustível notável. A Honda também não economizou na tecnologia, com recursos avançados de segurança e assistência ao motorista, conectividade aprimorada com Apple CarPlay e Android Auto e um compromisso com a sustentabilidade por meio de sua opção híbrida. O novo Honda Civic 2023 já está disponível nas concessionárias, pronto para definir um novo padrão de excelência em sedãs compactos. Este é um marco notável na jornada da Honda em direção ao futuro da mobilidade automotiva.",
+    "photoCover":"https://i.auto-bild.de/ir_img/2/6/8/7/5/7/1/civic-babcc1bdfa25a737.jpg?impolicy=video_desktop"
+  },
+  {
+    "id":"2",
+    "title":"Caminhonetes em Alta: Brasil Abraça a Versatilidade e o Estilo Off-Road",
+    "description":"O mercado automobilístico brasileiro está vivenciando uma notável ascensão no interesse por caminhonetes, com uma demanda crescente que abraça a versatilidade e o estilo off-road desses veículos. A busca por caminhonetes é impulsionada pela necessidade dos consumidores de um automóvel que possa lidar com desafios urbanos e aventuras off-road, tudo em um único pacote. Com vastas áreas naturais, trilhas e terrenos desafiadores, o Brasil se tornou o cenário ideal para as caminhonetes, que oferecem espaço para passageiros, capacidade de carga, tração nas quatro rodas e um nível crescente de conforto e tecnologia. Além disso, a economia de combustível e as opções mais sustentáveis contribuem para solidificar a posição das caminhonetes como preferidas dos consumidores brasileiros. O crescimento contínuo desse mercado está redefinindo o cenário automobilístico no país e parece destinado a continuar moldando as estradas brasileiras nos próximos anos.",
+    "photoCover":"https://www.autonocion.com/wp-content/uploads/2015/08/toyota-hilux-euro28.jpg"
+  },
+  {
+    "id":"3",
+    "title":"Lance Stroll fica fora do GP de Singapura após acidente durante classificação",
+    "description":"Lance Stroll, piloto da Aston Martin, foi forçado a se retirar do Grande Prêmio de Singapura após um acidente durante a classificação, onde ele largaria na 20ª posição. A equipe e o piloto tomaram a decisão em conjunto, dando a Stroll mais tempo para se recuperar dos efeitos do impacto e permitindo que a Aston Martin faça os reparos necessários em seu carro. Devido ao regulamento da Fórmula 1, que proíbe substitutos após a classificação, a equipe competirá com apenas um carro, representada por Fernando Alonso. O chefe de equipe, Mike Krack, enfatizou a prioridade da recuperação de Stroll e sua concentração no retorno ao cockpit no Grande Prêmio do Japão, que acontecerá na próxima semana.",
+    "photoCover":"https://www.grandepremio.com.br/wp-content/uploads/2023/09/STROLL-2.png"
+  },
+  {
+    "id":"4",
+    "title":"Aston Martin DBX: O Supremo SUV de Luxo e Desempenho",
+    "description":"A Aston Martin está causando sensação no mundo automobilístico com o lançamento de seu novo SUV, o Aston Martin DBX. Este veículo combina a elegância distintiva da marca com um desempenho excepcional e uma série de recursos inovadores. Seu design apresenta linhas aerodinâmicas e proporções elegantes, enquanto o interior luxuoso oferece materiais de alta qualidade e tecnologia de última geração. Sob o capô, um motor V8 biturbo de 4.0 litros proporciona potência abundante, tornando cada viagem uma experiência emocionante. Com tração nas quatro rodas e recursos avançados de assistência ao motorista, o DBX oferece segurança e desempenho em igual medida. Disponível em concessionárias de todo o mundo, o Aston Martin DBX é a escolha definitiva para aqueles que buscam luxo, estilo e desempenho excepcionais em um SUV de prestígio.",
+    "photoCover":"https://www.motortrend.com/uploads/2022/10/2023-Aston-Martin-DBX-707-2.jpg"
+  },
+]
